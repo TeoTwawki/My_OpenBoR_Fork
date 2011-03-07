311ff66a9c757980110df907ea89a9e9bf860452
sumolx <sumolx@66a56ed8-9c4d-4037-9b94-b260081fa8d2>
2011-03-07 18:48:19 +0000

    build.bat now uses win-sdk to build executable.
    git-svn-id: http://svn.code.sf.net/p/openbor/tools/tools@3162 66a56ed8-9c4d-4037-9b94-b260081fa8d2

--- borpak/source/build.bat
+++ borpak/source/build.bat
@@ -1,5 +1,17 @@
-:: Compiles borpak for Windows using MinGW.
-:: 
-:: You must have the GCC compiler from MinGW in your PATH for this to work.
+@rem
+@rem OpenBOR - http://www.LavaLit.com
+@rem -----------------------------------------------------------------------
+@rem Licensed under the BSD license, see LICENSE in OpenBOR root for details.
+@rem
+@rem Copyright (c) 2004 - 2011 OpenBOR Team
+@rem
 
-gcc -o borpak.exe borpak.c stristr.c scandir.c
+@rem ----------------------- Bash NIX Shell Scripts ------------------------
+
+@setlocal
+@echo off
+set BUILDBATCH=1
+set TOOLS=../../bin;../../win-sdk/bin
+set PATH=%TOOLS%;%PATH%
+bash.exe build.sh win
+@endlocal

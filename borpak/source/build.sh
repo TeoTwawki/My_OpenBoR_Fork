bb6b26392427029db5081c472fe25455dcbbde10
sumolx <sumolx@66a56ed8-9c4d-4037-9b94-b260081fa8d2>
2011-03-07 00:06:09 +0000

    build.sh now requires argument for targeted platform.  The script will auto detect which Host it is running on and use the appropriate SDK(s)
    git-svn-id: http://svn.code.sf.net/p/openbor/tools/tools@3154 66a56ed8-9c4d-4037-9b94-b260081fa8d2

--- borpak/source/build.sh
+++ borpak/source/build.sh
@@ -1,26 +1,42 @@
 #!/bin/sh
-# Compiles borpak for the Linux platform. Also works with Cygwin on Windows, 
-# although I don't know why you'd be using Cygwin instead of MinGW.
+# Compiles borpak for the following platforms...
+# Windows (MinGW), Mac OS X and Linux
 
-PLATFORM=$1
+TARGET_PLATFORM=$1
+HOST_PLATFORM=$(uname -s)
 
-if [ `echo $PLATFORM | grep -o "win"` ]; then
+if [ $# -ne 1 ]; then
+  echo "Usage: '`basename $0` mac' == Mac OS X"
+  echo "\t\t win  == Windows"
+  echo "\t\t lin  == Linux"
+  exit 1
+fi
+
+# Target is Windows
+if [ `echo $TARGET_PLATFORM | grep -o "win"` ]; then
   EXTENSION=".exe"
   SOURCE="scandir.c "
+  if [ `echo $HOST_PLATFORM | grep -o "Darwin"` ]; then
+    CC="i386-mingw32-"
+    PATH="$PATH:/usr/local/i386-mingw32-4.3.0/bin"
+  elif [ `echo $HOST_PLATFORM | grep -o "Linux"` ]; then
+    CC="i586-mingw32msvc-"
+  fi
 fi
 
-if [ `echo $PLATFORM | grep -o "macwin"` ]; then
-  CC="i386-mingw32-"
-  PATH="$PATH:/usr/local/i386-mingw32-4.3.0/bin"
-elif [ `echo $PLATFORM | grep -o "maclin"` ]; then
-  CC="i386-linux-"
-  PATH="$PATH:/usr/local/i386-linux-4.1.1/bin"
-elif [ `echo $PLATFORM | grep -o "mac"` ]; then
-  CFLAGS+="-arch x86_64 -arch i386 -arch ppc"
+# Target is Linux
+if [ `echo $TARGET_PLATFORM | grep -o "lin"` ]; then
+  if [ `echo $HOST_PLATFORM | grep -o "Darwin"` ]; then
+    CC="i386-linux-"
+    PATH="$PATH:/usr/local/i386-linux-4.1.1/bin"
+  fi
 fi
 
-if [ `echo $PLATFORM | grep -o "linwin"` ]; then
-  CC="i586-mingw32msvc-"
+# Target is Mac OS X
+if [ `echo $TARGET_PLATFORM | grep -o "mac"` ]; then
+  if [ `echo $HOST_PLATFORM | grep -o "Darwin"` ]; then
+    CFLAGS+="-arch x86_64 -arch i386 -arch ppc"
+  fi
 fi
 
 TARGET="borpak"
@@ -30,4 +46,3 @@ SOURCE="$SOURCE borpak.c stristr.c"
 
 $RM $TARGET$EXTENSION *.o
 $CC $CFLAGS -o $TARGET$EXTENSION $SOURCE
-
